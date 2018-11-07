eb706ba6708903596f4591bfe3c2954ea62793f7
Channaphat Taweesuksirianan <channaphat_t@BKC.CO.TH>
2018-11-07 14:16:49 +0700

    แก้ไข error ของ article

--- src/js/app.js
+++ src/js/app.js
@@ -3,82 +3,98 @@ App = {
       contracts: {},
       account: 0x0,
 
-      init: function() {
-  
-      //     var articleRow = $('#articlesRow');
-      //     var articleTemplate = $('#articleTemplate');
-  
-      //     articleTemplate.find('.panel-title').text('สินค้าชิ้นที่ 1');
-      //     articleTemplate.find('.article-description').text('รายละเอียดสินค้าชิ้นที่ 1');
-      //     articleTemplate.find('.article-price').text('10.24');
-      //     articleTemplate.find('.article-seller').text('ซื้อผู้ขาย');
-  
-      //     articleRow.append(articleTemplate.html());
-  
-          return App.initWeb3();
+      init: function () {
+
+            //     var articleRow = $('#articlesRow');
+            //     var articleTemplate = $('#articleTemplate');
+
+            //     articleTemplate.find('.panel-title').text('สินค้าชิ้นที่ 1');
+            //     articleTemplate.find('.article-description').text('รายละเอียดสินค้าชิ้นที่ 1');
+            //     articleTemplate.find('.article-price').text('10.24');
+            //     articleTemplate.find('.article-seller').text('ซื้อผู้ขาย');
+
+            //     articleRow.append(articleTemplate.html());
+
+            return App.initWeb3();
       },
-  
-      initWeb3: function() {
-            App.web3Provider  = new Web3.providers.HttpProvider('http://localhost:7545');
-             
-    
-             web3 = new Web3(App.web3Provider);
-    
-             App.displayAccountInfo();
-    
-             return App.initContract();
-    },
 
-    displayAccountInfo: function() {
+      initWeb3: function () {
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
 
-      web3.eth.getCoinbase(function(err, balance){
-            if(err === null){
-                  console.log("No error");
 
-                  App.account = balance;
-                  console.log(balance);
-                  $('#account').text(balance);
+            web3 = new Web3(App.web3Provider);
 
-                  web3.eth.getBalance(balance, function(err, balance){
-                        if(err === null){
-                              console.log(balance, " Wei");
-                              var ether = web3.fromWei(balance, "ether");
-                              $("#accountBalance").text(ether + " Eth");
-                        } else {
-                              console.log(err);
-                        }
+            App.displayAccountInfo();
 
-                  })
-                  
-            } else {
-                  console.log(err);
-            }
-      })
+            return App.initContract();
+      },
 
-    },
-  
-     initContract: function() {
-      $.getJSON('ChainList.json', function(chainListArtifact) {
-            App.contracts.ChainList = TruffleContract(chainListArtifact);
-            App.contracts.ChainList.setProvider(App.web3Provider);
-            return App.reloadArticles();
-      });
-     },
+      displayAccountInfo: function () {
 
-     reloadArticles: function(){
-           App.displayAccountInfo();
-           $('#articlesRow').empty();
+            web3.eth.getCoinbase(function (err, balance) {
+                  if (err === null) {
+                        console.log("No error");
 
-           App.contracts.ChainList.deployed().then( function(instance){
+                        App.account = balance;
+                        console.log(balance);
+                        $('#account').text(balance);
+
+                        web3.eth.getBalance(balance, function (err, balance) {
+                              if (err === null) {
+                                    console.log(balance, " Wei");
+                                    var ether = web3.fromWei(balance, "ether");
+                                    $("#accountBalance").text(ether + " Eth");
+                              } else {
+                                    console.log(err);
+                              }
+
+                        })
+
+                  } else {
+                        console.log(err);
+                  }
+            })
+
+      },
+
+      initContract: function () {
+            $.getJSON('ChainList.json', function (chainListArtifact) {
+                  App.contracts.ChainList = TruffleContract(chainListArtifact);
+                  App.contracts.ChainList.setProvider(App.web3Provider);
+                  return App.reloadArticles();
+            });
+      },
+
+      reloadArticles: function () {
+            App.displayAccountInfo();
+            $('#articlesRow').empty();
+
+            App.contracts.ChainList.deployed().then(function (instance) {
                   return instance.getArticle();
-           }).then(function(article){
-                 console.log(article);
-           });
-     }
-  };
-  
-  $(function() {
-     $(window).load(function() {
-          App.init();
-     });
-  });
+            }).then(function (article) {
+                  console.log(article);
+
+                  var articleRow = $('#articlesRow');
+                  var articleTemplate = $('#articleTemplate');
+
+                  articleTemplate.find('.panel-title').text(article[1]);
+                  articleTemplate.find('.article-description').text(article[2]);
+                  articleTemplate.find('.article-price').text(web3.fromWei(article[3], "ether"));
+
+                  var seller = "";
+                  if (article[0] == App.account) {
+                        seller = "You";
+                  } else {
+                        seller = article
+                  }
+                  articleTemplate.find('.article-seller').text(seller);
+                  articleRow.append(articleTemplate.html());
+            });
+      }
+};
+
+$(function () {
+      $(window).load(function () {
+            App.init();
+      });
+});

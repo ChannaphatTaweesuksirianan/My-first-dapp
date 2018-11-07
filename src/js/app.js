157e76bee0b51428fa5be7261c4df66927ae92f2
Channaphat Taweesuksirianan <channaphat_t@BKC.CO.TH>
2018-11-07 11:45:06 +0700

    เขียนโค้ดใน initWeb3: function()

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,44 @@
 App = {
-     web3Provider: null,
-     contracts: {},
+      web3Provider: null,
+      contracts: {},
+      account: 0x0,
 
-     init: function() {
-          /*
-           * Replace me...
-           */
-
+      init: function() {
+  
+      //     var articleRow = $('#articlesRow');
+      //     var articleTemplate = $('#articleTemplate');
+  
+      //     articleTemplate.find('.panel-title').text('สินค้าชิ้นที่ 1');
+      //     articleTemplate.find('.article-description').text('รายละเอียดสินค้าชิ้นที่ 1');
+      //     articleTemplate.find('.article-price').text('10.24');
+      //     articleTemplate.find('.article-seller').text('ซื้อผู้ขาย');
+  
+      //     articleRow.append(articleTemplate.html());
+  
           return App.initWeb3();
-     },
-
-     initWeb3: function() {
-          /*
-           * Replace me...
-           */
-
-          return App.initContract();
-     },
-
+      },
+  
+      initWeb3: function() {
+            App.web3Provider  = new Web3.providers.HttpProvider('http://localhost:7545');
+             } 
+    
+             web3 = new Web3(App.web3Provider);
+    
+             App.displayAccountInfo();
+    
+             return App.initContract();
+    },
+  
      initContract: function() {
           /*
            * Replace me...
            */
      },
-};
-
-$(function() {
+  };
+  
+  $(function() {
      $(window).load(function() {
           App.init();
      });
-});
+  });
+  }
